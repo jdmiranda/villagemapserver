11f870f6f212150eebb559a6f89d4ce622fd5c72
Jeremy Miranda <jmiranda@nc2media.com>
2016-01-19 00:11:15 -0600

    added morgan and body-parser

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  users = require('./lib/users.js');
+
 
   app.get('/', function(req, res){
-    res.send('hello');
+    res.send('hello, roxanna');
   });
 
   app.listen(3000, function(){
     console.log('listening on port 3000');
   });
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+
+
+app.get('/users', function(req, res){
+  users.getUsers(function(err, result){
+    if (err){
+      return res.status(500).json({success:false, reason:err.message});
+    }
+    res.send({success:true, users: result});
+  });
+});
