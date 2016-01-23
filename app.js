d392cadc314731c59287695732dad9e3d13b0a17
Jeremy Miranda <jmiranda@nc2media.com>
2016-01-23 16:16:45 -0600

    all tests passing, still not groking everything

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 var express = require('express'),
-  app = express(),
-  bodyParser = require('body-parser'),
-  morgan = require('morgan'),
-  users = require('./lib/users.js');
+    app = express(),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    users = require('./lib/users');
+
+module.exports = app;
 
 
-  app.get('/', function(req, res){
-    res.send('hello, roxanna');
-  });
-
-  app.listen(3000, function(){
-    console.log('listening on port 3000');
-  });
-
-  app.use(morgan('dev'));
-  app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 
-app.get('/users', function(req, res){
-  users.getUsers(function(err, result){
-    if (err){
-      return res.status(500).json({success:false, reason:err.message});
+app.get('/users', function(req, res) {
+
+  users.getUsers(function(err, result) {
+    if (err) {
+      // just an example (we don't actually throw any errors in getUsers)
+      return res.status(500).json( { success: false, reason: err.message });
     }
-    res.send({success:true, users: result});
+
+    res.send({ success: true, users: result });
   });
+
+});
+
+app.get('/users/:id', function(req, res) {
+  var id = req.params.id;
+
+  users.getUser(id, function(err, result) {
+    if (err) {
+      // just an example (bad request)
+      return res.status(400).json( { success: false, reason: err.message });
+    }
+
+    if (!result) {
+      return res.status(404).json( { success: false, reason: 'user id not found' });
+    }
+
+    res.send({ success: true, user: result });
+  });
+
+});
+
+app.post('/users', function(req, res) {
+  var user = req.body;
+
+  users.addUser(user, function(err, result) {
+    if (err) {
+      // just an example (bad request) since the only error that we throw is if missing user name
+      return res.status(400).json( { success: false, reason: err.message });
+    }
+
+    res.send({ success: true, user: result });
+  });
+
 });
