748c8e8ce7c78066d87b7846c2105e605dd64710
Jeremy Miranda <jmiranda@nc2media.com>
2016-01-29 22:09:50 -0600

    working email

--- routes/email.js
+++ routes/email.js
@@ -2,24 +2,19 @@ var express = require('express');
 var router = express.Router();
 var sendgridAk = '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'
 var sendgrid  = require('sendgrid')(sendgridAk);
-
-router.post('/', function(req, res) {
-   var sender = req.body.sender;
+router.post('/', function(req, res, next) {
+  var sender = req.body.sender;
   var body = req.body.body;
   var subject = req.body.subject;
-//console.log(JSON.stringify(req.body));
-if (body != null){
   sendgrid.send({
     to:       'jeremy.d.miranda@gmail.com',
     from:     sender,
     subject:  subject,
     text:     body
   }, function(err, json) {
-    if (err) { return console.error(err); }
-    console.log(json);
+    if (err)
+      res.send(err);
+    res.json(json);
   });
-}
 });
-
-
 module.exports = router;
