a1c17454962b67973fd1fedc191963d7e2c3f404
Jeremy Miranda <jmiranda@nc2media.com>
2016-01-29 21:21:58 -0600

    should be working but it isnt

--- routes/email.js
+++ routes/email.js
@@ -1,29 +1,17 @@
 var express = require('express');
-
 var router = express.Router();
-//var email = new sendgrid.Email();
 var sendgridAk = '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'
 var sendgrid  = require('sendgrid')(sendgridAk);
-// sendgrid.send({
-//   to:       'jeremy.d.miranda@gmail.com',
-//   from:     'other@example.com',
-//   subject:  'Hello World',
-//   text:     'My first email through SendGrid.'
-// }, function(err, json) {
-//   if (err) { return console.error(err); }
-//   console.log(json);
-// });
 
-router.get('/:sender/:subject/:body', function(req, res, next) {
-  var sender = req.params['sender'];
-  var body = req.param['body'];
-  var subjectVillageName = req.params['subject'];
-  console.log("params:" +req.params);
+router.post('/', function(req, res, next) {
+   var sender = req.body.sender;
+  var body = req.body.body;
+  var subject = req.body.subject;
 
   sendgrid.send({
     to:       'jeremy.d.miranda@gmail.com',
     from:     sender,
-    subject:  subjectVillageName,
+    subject:  subject,
     text:     body
   }, function(err, json) {
     if (err) { return console.error(err); }
