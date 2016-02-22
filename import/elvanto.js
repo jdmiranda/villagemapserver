c7006b27188130845e81434ba34e8d5d57d3a57e
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2016-02-22 16:45:24 -0600

    getting the cron job for elvanto

---
+++ import/elvanto.js
@@ -0,0 +1,31 @@
+var Neighborhood = require('../models/neighborhood.js');
+var options = {
+  host: 'api.elvanto.com',
+  port: 80,
+  path: '/v1/groups/getAll.json',
+  auth: 'gPUD4UXum3Ui5Nmuuxs0l3F2qkP9PjRW:x'
+};
+
+
+var processGroup = function(group){
+  NeighborHood.findById(group.id, function (err, post) {
+    if (err) return next(err);
+    console.log('found: ' + group.name);
+  });
+};
+
+var callback = function(response) {
+  var dataJson='';
+
+  response.on('data', function (chunk) {
+    dataJson += chunk;
+  });
+
+  response.on('end', function () {
+    dataJson = JSON.parse(dataJson);
+    dataJson.groups.group.forEach(processGroup);
+  });
+};
+
+
+var req = require('http').request(options, callback).end();
