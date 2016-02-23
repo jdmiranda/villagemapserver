9f822a5bbaecfcb3e13bf51e98337dc84c36caaa
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2016-02-23 14:26:47 -0600

    updater working

--- import/elvanto.js
+++ import/elvanto.js
@@ -1,4 +1,21 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku-app:journeychurch@ds051655.mongolab.com:51655/journeychurch', function(err){
+  if(err) {
+    console.log('connection error', err);
+  } else {
+    console.log('connection successful');
+  }
+});
+
 var Neighborhood = require('../models/neighborhood');
+var geocoderProvider = 'google';
+var httpAdapter = 'https';
+var extra = {
+  apiKey: 'AIzaSyA5Db5h9EXZQtKdc2puzX1PyoUxpyT3Ulk', // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
+
 var options = {
   host: 'api.elvanto.com',
   port: 80,
@@ -6,30 +23,62 @@ var options = {
   auth: 'gPUD4UXum3Ui5Nmuuxs0l3F2qkP9PjRW:x'
 };
 
-//console.log(Neighborhood.collection);
+var lookupAddress = function(neighborhood, address){
+  console.log('looking up new address ');
+  geocoder.geocode(address)
+  .then(function(res) {
+    neighborhood.lat = res[0].latitude,
+    neighborhood.lng = res[0].longitude
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+  return neighborhood;
+};
 
-// Neighborhood.findOne({'id':function (err, foundGroup){
-//   console.log('love me');
-//   if(err) return console.log(err);
-//   console.log(foundGroup);
-// });
-
-
+var createNeighborhood = function (group, address){
+  console.log( 'creating new neighborhood ' + group.name);
+  console.log('group')
+  if (typeof address == 'undefined'){
+    return;
+  };
+  var newNeighborhood = new Neighborhood({
+    id: group.id,
+    name: group.name,
+    address: address,
+    status: group.status
+  });
+  var updatedNeighborhood = lookupAddress(newNeighborhood, address);
+  newNeighborhood.save();
+  console.log('saved ' + newNeighborhood.name);
+};
 
 var processGroup = function(group){
-  //console.log( 'group id:' + group.id);
-  console.log(group.id);
-  Neighborhood.findOne({'id':group.id}, function (err, foundGroup){
-    console.log(foundGroup);
-    // if (err) {
-    //   console.log(err);
-    //   return;
-    // };
-  //  console.log('found: ' + group.name);
-// }, function(found){
-// console.log(found)
-});
+  if (group.name.includes("Neighborhood")) {
+    var groupAddress = group.meeting_address + "," + group.meeting_city + "," + group.meeting_postcode;
+    Neighborhood.findOne({'id':group.id}, function (err, foundGroup){
+      if (err) return  console.log(err);
+      if (foundGroup){
+        var saveGroup = foundGroup;
+        if (foundGroup.address != groupAddress){
+          console.log('old address: ' + foundGroup.address);
+          console.log('new address: ' + groupAddress);
+          saveGroup = lookupAddress(foundGroup, groupAddress);
+        }
+
+        saveGroup.name = group.name;
+        saveGroup.status = group.status;
+        saveGroup.address = groupAddress;
+        saveGroup.save();
+      }
+      else{
+        if (group.meeting_address == '') return;
+        createNeighborhood(group, groupAddress);
+      }
+    });
+  };
 };
+
 
 var callback = function(response) {
   var dataJson='';
@@ -42,6 +91,7 @@ var callback = function(response) {
     dataJson = JSON.parse(dataJson);
     dataJson.groups.group.forEach(processGroup);
   });
+
 };
 
 
