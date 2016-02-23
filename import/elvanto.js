9805c30d5af4b33a3c1ec4ae5fddf80d87ca4175
Jeremy Miranda <Jeremy.D.Miranda@gmail.com>
2016-02-22 21:57:06 -0600

    still not working

--- import/elvanto.js
+++ import/elvanto.js
@@ -1,4 +1,4 @@
-var Neighborhood = require('../models/neighborhood.js');
+var Neighborhood = require('../models/neighborhood');
 var options = {
   host: 'api.elvanto.com',
   port: 80,
@@ -6,15 +6,29 @@ var options = {
   auth: 'gPUD4UXum3Ui5Nmuuxs0l3F2qkP9PjRW:x'
 };
 
+//console.log(Neighborhood.collection);
+
+// Neighborhood.findOne({'id':function (err, foundGroup){
+//   console.log('love me');
+//   if(err) return console.log(err);
+//   console.log(foundGroup);
+// });
+
+
 
 var processGroup = function(group){
-  Neighborhood.findById(group.id, function (err, post) {
-    if (err) {
-      console.log(err);
-      return;
-    };
-    console.log('found: ' + group.name);
-  });
+  //console.log( 'group id:' + group.id);
+  console.log(group.id);
+  Neighborhood.findOne({'id':group.id}, function (err, foundGroup){
+    console.log(foundGroup);
+    // if (err) {
+    //   console.log(err);
+    //   return;
+    // };
+  //  console.log('found: ' + group.name);
+// }, function(found){
+// console.log(found)
+});
 };
 
 var callback = function(response) {
